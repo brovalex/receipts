4d5aba39f6bd9169d6c674b9a9c19871ec32e872
Alex Brovkin <alex@webmovement.com>
2025-03-13 20:56:05 -0400

    more product search query tweaks

--- services/price_scraper/modules/reference_products_mapping.py
+++ services/price_scraper/modules/reference_products_mapping.py
@@ -8,7 +8,7 @@ REFERENCE_PRODUCTS = pd.DataFrame([
     {"id": 5, "name": "Cereal", "product_name": "Corn flakes cereal"},
     {"id": 6, "name": "Chia seeds", "product_name": "whole chia Seeds"},
     {"id": 7, "name": "Chips", "product_name": "Potato Chips"},
-    {"id": 8, "name": "Chocolate", "product_name": "irresitible chocolate bar"},
+    {"id": 8, "name": "Chocolate", "product_name": "irresistible chocolate bar"},
     {"id": 9, "name": "Chocolate chips", "product_name": "baking chocolate chips"},
     {"id": 10, "name": "Cookies", "product_name": "Cookies"},
     {"id": 11, "name": "Cornstarch", "product_name": "cornstarch baking"},
