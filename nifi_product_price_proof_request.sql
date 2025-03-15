0829c2db2d0589a4f42581d1a0dedb7630f7b875
Alex Brovkin <alex@webmovement.com>
2025-03-14 21:41:04 -0400

    queries work

---
+++ nifi_product_price_proof_request.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT id as reference_item_id
+FROM receipts_app.reference_item
+WHERE 
+  id NOT IN (
+    SELECT reference_item_id as id
+    FROM receipts_app.product_price_proof
+    WHERE 
+      created_at >= date_trunc('week', CURRENT_DATE) AND -- freshness check is for later
+      validated IS TRUE
+  )
+order by reference_item_id ASC;
---
+++ report.sql
@@ -0,0 +1,57 @@
+SET search_path TO receipts_app;
+WITH ClosestPriceProof AS (
+    SELECT 
+        ppp.id AS price_proof_id,
+        ppp.name AS price_proof_name,
+        ppp.reference_item_id,
+        ppp.price,
+        ppp.quantity,
+        ppp.unit_of_measure,
+        ppp.created_at AS price_proof_date,
+        e.id AS expense_id,
+        ROW_NUMBER() OVER (
+            PARTITION BY e.id, ppp.reference_item_id 
+            ORDER BY ABS(EXTRACT(EPOCH FROM (e.created_at - ppp.created_at)))
+        ) AS rn
+    FROM product_price_proof ppp
+    JOIN reference_item ri ON ppp.reference_item_id = ri.id
+    JOIN product p ON p.reference_item_id = ri.id
+    JOIN expense e ON e.product_id = p.id
+    WHERE ppp.reference_item_id IS NOT NULL AND ppp.validated IS TRUE
+),
+report AS (
+    SELECT 
+        e.id AS expense_id,
+        e.receipt_id,
+        e.price_each,
+        e.quantity,
+        e.price_each * e.quantity AS total_price,
+        e.created_at AS expense_date,
+        p.id AS product_id,
+        p.name AS product_name,
+        p.weight AS product_weight,
+        p.unit_of_measure AS product_unit_of_measure,
+        ri.id AS reference_item_id,
+        ri.name AS reference_item_name,
+        cpp.price_proof_id,
+        cpp.price_proof_name,
+        cpp.price AS proof_price,
+        cpp.quantity AS proof_quantity,
+        cpp.unit_of_measure AS proof_unit_of_measure,
+        cpp.price_proof_date
+    FROM expense e
+    JOIN product p ON e.product_id = p.id
+    JOIN reference_item ri ON p.reference_item_id = ri.id
+    LEFT JOIN (SELECT * FROM ClosestPriceProof WHERE rn = 1) cpp 
+        ON ri.id = cpp.reference_item_id AND e.id = cpp.expense_id
+    WHERE cpp.price_proof_id IS NULL
+    ORDER BY e.created_at DESC
+)
+SELECT
+* 
+FROM report
+-- WHERE price_proof_id IS NULL
+-- GROUP BY reference_item_id
+;
+
+
